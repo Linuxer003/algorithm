478406503455fb097ec45087d733d8a984879529
wgh <1127003038@qq.com>
2020-12-07 15:47:40 +0800

    Initial commit

---
+++ src/leetcode/reverseMatric.h
@@ -0,0 +1,18 @@
+//
+// Created by 王国洪 on 2020/12/7.
+//
+
+#ifndef CHERNO_REVERSEMATRIC_H
+#define CHERNO_REVERSEMATRIC_H
+
+#include <iostream>
+#include <vector>
+
+class reverseMatric {
+public:
+    int matrixScore(std::vector<std::vector<int>>& A);
+    int matrixScore2(std::vector<std::vector<int>>& A);
+};
+
+
+#endif //CHERNO_REVERSEMATRIC_H
